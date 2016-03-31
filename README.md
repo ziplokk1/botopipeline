c8ae40ae7aa56c24903d141e9a1f6088cf209e52
Mark <ziplokk.mark.sanders@gmail.com>
2016-03-31 10:25:15 -0500

    Added support for writing to multiple queues

--- README.md
+++ README.md
@@ -9,8 +9,8 @@ The pipeline will write json serialized items to the queue specified in your scr
 To configure the pipeline add the following lines to settings.py
 
 ```python
-AWS_ACCESS_KEY = your_access_key
-AWS_SECRET_KEY = your_secret_key
-AWS_REGION = your_aws_region
-SQS_QUEUE_NAME = your_sqs_queue_name
+AWS_ACCESS_KEY = '<your_access_key>'
+AWS_SECRET_KEY = '<your_secret_key>'
+AWS_REGION = '<your_aws_region>'
+SQS_QUEUE_NAME = ['<your_sqs_queue_name>']
 ```
--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 import json
+from collections import deque
 
 from boto import sqs
 from boto.sqs import message
@@ -11,19 +12,19 @@ class BotoPipeline(object):
         self.aws_access_key_id = crawler.settings.get('AWS_ACCESS_KEY')
         self.aws_secret_key = crawler.settings.get('AWS_SECRET_KEY')
         self.region = crawler.settings.get('AWS_REGION')
-        self.queue_name = crawler.settings.get('SQS_QUEUE_NAME')
+        self.queue_names = crawler.settings.get('SQS_QUEUE_NAMES')
         if not self.aws_access_key_id:
             raise ValueError('please set AWS_ACCESS_KEY in settings')
         elif not self.aws_secret_key:
             raise ValueError('please set AWS_SECRET_KEY in settings')
         elif not self.region:
             raise ValueError('please set AWS_REGION in settings')
-        elif not self.queue_name:
-            raise ValueError('please set SQS_QUEUE_NAME in settings')
+        elif not self.queue_names:
+            raise ValueError('please set SQS_QUEUE_NAMES in settings')
         self.connection = sqs.connect_to_region(self.region,
                                                 aws_access_key_id=self.aws_access_key_id,
                                                 aws_secret_access_key=self.aws_secret_key)
-        self.queue = self.connection.get_queue(self.queue_name)
+        self.queues = deque([(self.connection.get_queue(q), q) for q in self.queue_names])
 
     def process_item(self, item, spider):
         boto_item = item.__dict__.get('_values')
@@ -40,7 +41,14 @@ class BotoPipeline(object):
         msg = message.Message()
         msg.set_body(json.dumps(boto_item))
         msg.message_attributes = msg_attrs
-        self.queue.write(msg)
+
+        # Write to one queue, then rotate to the next.
+        # This way you can have multiple queues sharing the
+        # load and multiple clients reading.
+        q, name = self.queues[0]
+        q.write(msg)
+        self.crawler.stats.inc_value('SQS_writes/%s' % name)
+        self.queues.rotate(-1)
 
     @classmethod
     def from_crawler(cls, crawler):
