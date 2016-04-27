8e67b26f9ce3161d3501281c5520b78052e8ad8b
Mark <ziplokk.mark.sanders@gmail.com>
2016-04-27 11:43:00 -0500

    Prepping for pypi deployment

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+REQUIREMENTS = [
+    'boto'
+]
+
+setup(
+    name='scrapy-sqs-pipeline',
+    version=version,
+    packages=find_packages(),
+    url='https://github.com/ziplokk1/botopipeline',
+    license='LICENSE.txt',
+    author='Mark Sanders',
+    author_email='sdscdeveloper@gmail.com',
+    install_requires=REQUIREMENTS,
+    description='Write scraped items to Amazon SQS.'
+)
